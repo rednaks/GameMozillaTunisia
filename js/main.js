faac699927eea6daf65b2dad0af4f0f8e9167ee3
Zaafouri Amine <zaafouri.amine@gmail.com>
2012-08-06 16:39:51 +0100

    initialize the hidden/shown boxes

--- js/main.js
+++ js/main.js
@@ -8,13 +8,19 @@ canvas.width = 400;
 var context = canvas.getContext("2d");
 //the context is the object that allow us to draw in the canvas .. we will use 2d not the 3d
 
-var maxCells = 3;
-//the size of the grid
-
 context.strokeStyle = "red";
 //choose a color
 
-for(var i = 0; i < maxCells; i++)
-	for(var j = 0; j < maxCells; j++)
+for(var i = 0; i < 3; i++)
+	for(var j = 0; j < 3; j++)
 		context.strokeRect(i * 50, j * 50, 50, 50);
 //we draw the grid (9 rectangles)
+
+var matrix[];
+for (var i = 0; i < 3; i++){
+	matrix[i] = [];
+	for(var j = 0; j < 3; j++)
+		matrix[][] = -1;
+}
+matrix[1][1] = 0;
+//we initialize that all the boxes are hidden except the slogan of Mozilla Tunisia
